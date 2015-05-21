306db1ddeb0d329eeec7f474c505c9643f7ecaa5
Brian Haas <brianjohnhaas@gmail.com>
2015-05-21 08:49:49 -0400

    update, make symlink to galaxy data area for FIWeb

--- FusionInspector_wrapper.pl
+++ FusionInspector_wrapper.pl
@@ -3,9 +3,14 @@
 use strict;
 use warnings;
 
+my $CTAT_GALAXY_DATA = $ENV{CTAT_GALAXY_DATA} or die "Error, need CTAT_GALAXY_DATA env var set to dir under apache for linking to the galaxy data area";
+
 my @x = @ARGV;
 
-my $out_dir = pop @x;
+my $galaxy_data_file = pop @x;
+
+my $out_dir = $galaxy_data_file;
+$out_dir =~ s/\.dat$/_files/ or die "Error, cannot convert .dat to _files dir for $out_dir";
 
 my $cmd = join(" ", @x);
 &process_cmd($cmd);
@@ -13,6 +18,7 @@ my $cmd = join(" ", @x);
 &process_cmd("mkdir -p $out_dir");
 
 my @files = qw(
+cytoBand.txt
 FInspector.fa
 FInspector.fa.fai
 FInspector.fusion_predictions.txt
@@ -23,14 +29,26 @@ FInspector.junction_reads.bam.bed.sorted.bed.gz
 FInspector.junction_reads.bam.bed.sorted.bed.gz.tbi
 FInspector.junction_reads.bam.bed.sorted.bed.gz
 FInspector.junction_reads.bam.bed.sorted.bed.gz.tbi
-FInspector.gmap_trinity_GG.fusions.gff3.bed.sorted.bed.gz
-FInspector.gmap_trinity_GG.fusions.gff3.bed.sorted.bed.gz.tbi
+
 );
 
+=TrinGG
+FInspector.gmap_trinity_GG.fusions.gff3.bed.sorted.bed.gz
+FInspector.gmap_trinity_GG.fusions.gff3.bed.sorted.bed.gz.tbi
+=cut
 
 foreach my $file (@files) {
     &process_cmd("cp $file $out_dir/$file");    
 }
+
+# make a link to the out_dir from the apache data area
+my $token = sprintf("%x", int(rand(time)) . int(rand(time)));
+open (my $ofh, ">$out_dir/symtok.txt") or die "Error, cannot write to $out_dir/symtok.txt";
+print $ofh $token;
+close $ofh;
+
+my $link_cmd = "ln -s $out_dir $CTAT_GALAXY_DATA/$token";
+&process_cmd($link_cmd);
 
 
 exit(0);
